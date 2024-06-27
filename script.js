902264572acb39797958da38950243e42fac7364
pedrofabianohub <pedrofabiano421@gmail.com>
2024-06-27 17:10:29 -0300

    Frontend

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const chatMessages = document.querySelector('.chat-messages');
 const userInput = document.getElementById('user-input');
 const sendBtn = document.getElementById('send-btn');
-const marked = require('marked')
+import { Marked } from "marked";
 
 sendBtn.addEventListener('click', sendMessage);
 
