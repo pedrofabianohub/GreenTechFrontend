6ff2ae4100a0ac8f4bd2e05a533085357784f855
pedrofabianohub <pedrofabiano421@gmail.com>
2024-06-27 16:06:32 -0300

    Frontend

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ const chatMessages = document.querySelector('.chat-messages');
 const userInput = document.getElementById('user-input');
 const sendBtn = document.getElementById('send-btn');
 
+// Event listener para enviar mensagem ao clicar no botão
 sendBtn.addEventListener('click', sendMessage);
 
+// Event listener para enviar mensagem ao pressionar Enter
 userInput.addEventListener("keyup", function(event) {
   if (event.keyCode === 13) { 
     event.preventDefault();
@@ -11,44 +13,52 @@ userInput.addEventListener("keyup", function(event) {
   }
 });
 
-function sendMessage() {
+// Função para enviar mensagem ao servidor e exibir resposta
+async function sendMessage() {
   const message = userInput.value;
   if (message.trim() === '') return;
 
   displayMessage(message, 'user');
   userInput.value = '';
 
-  sendMessageToServer(message)
-    .then(botResponse => {
-      displayMessage(botResponse, 'bot');
-    })
-    .catch(error => {
-      console.error('Erro ao enviar mensagem para o servidor:', error);
-      displayMessage('Erro ao conectar ao chatbot. Por favor, tente novamente.', 'bot');
-    });
+  try {
+    const botResponse = await sendMessageToServer(message);
+    displayMessage(botResponse, 'bot');
+  } catch (error) {
+    console.error('Erro ao enviar mensagem para o servidor:', error);
+    displayMessage('Erro ao conectar ao chatbot. Por favor, tente novamente.', 'bot');
+  }
 }
 
+// Função para exibir mensagens no chat
 function displayMessage(message, sender) {
   const messageElement = document.createElement('div');
   messageElement.classList.add('message');
 
-  if (sender === 'user') { 
-    messageElement.textContent = message; // Mensagem do usuário como texto
-    messageElement.classList.add('user-message'); 
-  } else if (sender === 'bot') { 
-    messageElement.innerHTML = marked.parse(message); // Mensagem do bot com Markdown
-    messageElement.classList.add('bot-message'); 
+  if (sender === 'user') {
+    messageElement.textContent = message;
+    messageElement.classList.add('user-message');
+  } else if (sender === 'bot') {
+    const formattedMessage = formatBotResponse(message);
+    messageElement.innerHTML = formattedMessage;
+    messageElement.classList.add('bot-message');
   }
 
   chatMessages.appendChild(messageElement);
   chatMessages.scrollTop = chatMessages.scrollHeight;
 }
 
+function formatBotResponse(message) {
+  return `<p>${message}</p>`;
+}
+
+// URL base da API
 const apiBaseUrl = 'https://green-tech-six.vercel.app/';
 
+// Função para enviar mensagem para o servidor
 async function sendMessageToServer(message) {
   try {
-    const response = await fetch(`${apiBaseUrl}`, {
+    const response = await fetch(`${apiBaseUrl}mensagem`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
